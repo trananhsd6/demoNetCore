a10b3c3c2d59e0f810deea3223cf37e20d06359a
Tran Anh <tta@123>
2020-02-14 14:39:15 +0700

    Add project files.

---
+++ demoNetCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demoNetCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
