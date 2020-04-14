ae8efbb9d71fb6bab7daf1e3bac010f9741ca940
Tran Anh <tta@123>
2020-04-14 18:20:40 +0700

    Add api authenticate and Regester

---
+++ demoNetCore.Application/System/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using demoNetCore.ViewModel.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoNetCore.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authencate(LoginRequest request);
+        Task<bool> Register(RegisterRequest request);
+    }
+}
