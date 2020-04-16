3dbe254ba04fb2176f401413ae0d27151e592712
Tran Anh <tta@123>
2020-04-16 10:26:51 +0700

    Add login page

---
+++ demoNetCore.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using demoNetCore.ViewModel.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demoNetCore.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
